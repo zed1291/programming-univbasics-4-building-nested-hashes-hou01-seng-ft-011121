9f4aae920fcb44457d33687e48bc11546f070bd0
zed <zed@kolk.me>
2020-12-17 22:24:24 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,6 +21,8 @@ def monopoly_with_third_tier
         :reading_railroad => {},
         :pennsylvania_railroad => {},
         :b_and_o_railroad => {},
+        :shortline_railroad => {},
+        # :b_and_o_railroad => {},
       }
     }
   }
