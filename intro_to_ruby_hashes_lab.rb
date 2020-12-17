e546f623b3bc320fef711fbc99df0b2497e010fc
zed <zed@kolk.me>
2020-12-17 22:23:39 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,7 +17,10 @@ def monopoly_with_third_tier
     :railroads => {
       :pieces => 4,
       :rent_in_dollars => {:one_piece_owned => 25, :two_pieces_owned => 50, :three_pieces_owned => 100, :four_pieces_owned => 200},
-      :names => {:reading_railroad => {}}
+      :names => {
+        :reading_railroad => {},
+        :pennsylvania_railroad => {},
+      }
     }
   }
 
