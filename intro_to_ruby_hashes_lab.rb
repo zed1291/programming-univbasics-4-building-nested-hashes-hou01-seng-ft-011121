2dcbedb0e56dae455b5f5ae6b5c78ec940e17fdf
zed <zed@kolk.me>
2020-12-17 21:57:58 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -7,7 +7,7 @@ end
 
 def monopoly_with_second_tier
   monopoly = {
-    :railroads => {:pieces => 4, :rent_in_dollars => {}}
+    :railroads => {:pieces => 4, :rent_in_dollars => {}, :name => {}}
   }
 
 end
